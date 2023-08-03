8bc1649f17284fa19dec1994f4bbfa35946a6eeb
z-ovc <zzz_oveysi@yahoo.com>
2023-08-03 16:26:21 +0330

    chatbot backend connection added

--- backend.py
+++ backend.py
@@ -3,10 +3,21 @@ import openai
 
 class ChatBot():
     def __init__(self) -> None:
-        openai.api_key = ""
+        openai.api_key = "*sk*-jocTOJLcDM8nA0PXmJ*45T3Blb*kFJdg5ONU2*q1LK6Q4dfLZDE"
 
     def get_response(self,user_input):
-        response = openai.completion.create(
+        response = openai.Completion.create(
             engine = "text-davinci-003",
-            prompt = user_input
-        )
+            prompt = user_input,
+            max_tokens = 4000,
+            temperature = 0.5
+        ).choices[0].text
+        return response
+    
+
+
+if __name__=="__main__":
+    chatbot = ChatBot()
+    response = chatbot.get_response("a joke about dogs")
+    print(response)
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from PyQt6.QtWidgets import QMainWindow, QTextEdit, QPushButton, QApplication
+from PyQt6.QtWidgets import QMainWindow, \
+QTextEdit, QPushButton, QApplication, QLineEdit
 import sys
+from backend import ChatBot
+import threading
 
 class ChatBotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
-        self.setMinimunSize(500,700)
+        self.chatbot = ChatBot()
+        self.setMinimumSize(800,900)
         #add Chat area 
         self.chat_area = QTextEdit(self)
         self.chat_area.setGeometry(10,10,480,320)
@@ -15,15 +18,24 @@ class ChatBotWindow(QMainWindow):
         #add the input frame
         self.input_field = QLineEdit(self)
         self.input_field.setGeometry(10,340,480,50)
-
+        self.input_field.returnPressed.connect(self.send_message)
+ 
         #add button
         self.button = QPushButton("Send",self)
         self.button.setGeometry(500,340,100,50)
+        self.button.clicked.connect(self.send_message)
         self.show()
 
-class ChatBot():
-    pass
+    def send_message(self):
+        user_input = self.input_field.text().strip()
+        self.chat_area.append(f"<p style='color:#333333'>me: {user_input} </p>")
+        self.input_field.clear()
 
+        thread = threading.Thread(target = self.get_bot_response, args = (user_input,))
+        thread.start()
+    def get_bot_response(self,user_input):
+        response = self.chatbot.get_response(user_input)
+        self.chat_area.append(f"<p style='color:#333333'; background-color:#E9E9E9'> Bot: {response} </p>")
 
 app = QApplication(sys.argv)
 main_window = ChatBotWindow()
